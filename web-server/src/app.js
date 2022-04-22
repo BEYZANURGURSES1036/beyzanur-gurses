8eeed4b1e1b4d2353c8dd6eff3a2b8d0e275f2b8
Beyzanur Gürses <beyzanurgurses1036@gmail.com>
2022-04-22 10:33:14 +0300

    upload

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,19 +1,26 @@
 const express = require("express");
-
-
 const path=require('path')
 console.log(__dirname)
 console.log(__filename)
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
-console.log(publicDirectoryPath)
 app.use(express.static(publicDirectoryPath)) //kullanacğım statik dosya
-homepage (app.com), help (app.com/help), about (app.com/about)
+//homepage (app.com), help (app.com/help), about (app.com/about)
 
 
 app.get('', (req, res) => {
     res.send()
+})
+
+app.get('/', (req, res) => {
+    
+    console.log(req.query) 
+    res.send(
+        {
+            products: [],
+           
+        })
 })
 
 
