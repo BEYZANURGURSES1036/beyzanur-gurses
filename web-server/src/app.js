8c0ae9ce634d12b39c2b6bd3ffb4a2f10b614ef9
Beyzanur Gürses <beyzanurgurses1036@gmail.com>
2022-04-22 11:02:36 +0300

    commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,21 @@
 const express = require("express");
 const path=require('path')
-console.log(__dirname)
-console.log(__filename)
+const geocode = require('./utils/geocode')
+const weather = require('./utils/weather')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname,'../public')
-app.use(express.static(publicDirectoryPath)) //kullanacğım statik dosya
-//homepage (app.com), help (app.com/help), about (app.com/about)
+const publicDirectoryPath = path.join("../web-server/src");
+app.use(express.static(publicDirectoryPath)) 
 
 
-app.get('', (req, res) => {
-    res.send()
+app.get('test_text', (req, res) => {
+    res.send("18360859051");
 })
-
-app.get('/', (req, res) => {
-    
-    console.log(req.query) 
-    res.send(
-        {
-            products: [],
-           
-        })
+app.get('test_html', (req, res) => {
+    res.send("Beyzanur Gürses");
+})
+app.get('test_json', (req, res) => {
+    res.send("Beyzanur Gürses");
 })
 
 
@@ -29,4 +24,6 @@ const port =process.env.PORT || 3000
 app.listen(port, () => {
     console.log('this is ' +  port)
 })
+
+
 
