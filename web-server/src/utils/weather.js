9dc76ddf0607d330314487978c82dae84c68407d
Beyzanur Gürses <beyzanurgurses1036@gmail.com>
2022-04-22 10:44:03 +0300

    commit

--- web-server/src/utils/weather.js
+++ web-server/src/utils/weather.js
@@ -1,19 +1,14 @@
 const request = require('request')
 
-const forecast = (latitude, longitude, callback) => {
+const weather = (latitude, longitude, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=........&query=' + latitude + ',' + longitude + '&units=m'
 
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to weather service', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, 'Hava Durumu Açıklaması: ' + body.current.weather_descriptions[0] + ',\nHava Sıcaklığı : ' 
-            + body.current.temperature + ' derece' + ',\nHissedilen sıcaklık: ' + body.current.pressure + ' derece')
-        }
+    request( (error, { body }) => {
+            callback(undefined, 'Hava Durumu Açıklaması: ' + body.current.weather_descriptions[0] + ',\n Sıcaklığı : ' 
+            + body.current.temperature + ' derece' + ',\nBasınç ' + body.current.pressure + '\n Nem '+ body.current.humidity )
+      
     })
 }
 
-module.exports = forecast
+module.exports = weather
